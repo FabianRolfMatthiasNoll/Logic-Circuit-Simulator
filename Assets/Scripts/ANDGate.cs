5678840682fe10de6d0968d22b31c7011f10ce76
Fabian Noll <Fabian.Noll@googlemail.com>
2023-07-16 21:27:41 +0200

    Add simple logic Gate and first UI
    No workable DEMO or anything close available

---
+++ Assets/Scripts/ANDGate.cs
@@ -0,0 +1,15 @@
+﻿
+public class AndGate : Gate
+{
+    public override bool GetOutput()
+    {
+        if (inputs.Count == 0) return false;
+        
+        foreach (var input in inputs)
+        {
+            if (!input.GetOutput()) return false;
+        }
+
+        return true;
+    }
+}
---
+++ Assets/Scripts/ConstantGate.cs
@@ -0,0 +1,14 @@
+﻿public class ConstantGate : Gate
+{
+    public bool ConstantOutput { get; set; }
+
+    public ConstantGate(bool output)
+    {
+        ConstantOutput = output;
+    }
+
+    public override bool GetOutput()
+    {
+        return ConstantOutput;
+    }
+}
